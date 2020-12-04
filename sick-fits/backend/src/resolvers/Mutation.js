56e8d1831093b8aa1c0697b4eeb267dab50f115b
Liynx <liamt.hyatt@gmail.com>
2020-12-04 14:59:23 -0800

    making first custom resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,26 @@
-const Mutations = {};
+const Mutations = {
+  //wait for item to be created before returning from db
+  async createItem(parent, args, ctx, info) {
+    //Todo: Check if they are logged in
+    
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        // Spread the agruments into the data to add to that db
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+  // createDog(parent, args, ctx, info){
+  //   global.dogs = global.dogs || [];
+  //   //create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+
+  //   //console.log(args)
+  // }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,21 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  //Dog Example:
+  // dogs: function(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || []
+  //   return global.dogs
+  // }
+
+  // Writing custom query:
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items;
+  // }
+
+  //forward query directly to prisma so it uses the query defined there
+  items: forwardTo('db')
+
+};
 
 module.exports = Query;
